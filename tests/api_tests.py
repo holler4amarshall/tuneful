4ddcb5e2b9db5c3e36bf5b169fe44fce034d8ced
holly <holler4amarshall@hotmail.com>
2016-12-20 09:01:29 +0000

    update tests to verify getting 2 songs

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -30,16 +30,51 @@ class TestAPI(unittest.TestCase):
         
         
     def test_get_empty_list_of_songs(self):
-        """ Getting songs from an empty database """
+        """ Getting songs - fail: DB empty"""
+        #No Song Data is added to DB
+        #query api
         response = self.client.get("/api/songs",
         headers=[("Accept", "application/json")]
         )
-    
+        #assert api response
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, "application/json")
-    
         data = json.loads(response.data.decode("ascii"))
         self.assertEqual(data, [])
+        
+       
+    def test_get_songs(self):
+        """ Getting songs - success """
+        #Add 2 Songs to DB with File Data
+        file1 = models.File(filename="Awake.mp3")
+        file2 = models.File(filename="Montana.mp3")
+        song1 = models.Song(name="Awake", file=file1)
+        song2 = models.Song(name="Montana", file=file2)
+        
+        session.add_all([song1, song2])
+        session.commit()
+        
+        #query api
+        response = self.client.get("/api/songs")
+        print(response)
+        
+        #assert api response contains expected response
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(len(data), 2)
+        
+        #assert response contains expected songs/file
+        song1 = data[0]
+        print(song1)
+        self.assertEqual(song1["songname"], "Awake")
+        self.assertEqual(song1["file"]["filename"], "Awake.mp3")
+        self.assertEqual(song1["file"]["id"], song1["id"])
+
+        song2 = data[1]
+        self.assertEqual(song2["songname"], "Montana")
+        self.assertEqual(song2["file"]["filename"], "Montana.mp3")
+        self.assertEqual(song1["file"]["id"], song2["id"])
 
     def tearDown(self):
         """ Test teardown """
