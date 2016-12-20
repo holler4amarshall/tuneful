f6166164d419bdba864cbcacd82e04e981d10567
holly <holler4amarshall@hotmail.com>
2016-12-20 07:35:05 +0000

    create api for songs to return list of songs from DB

--- tuneful/api.py
+++ tuneful/api.py
@@ -12,3 +12,21 @@ from .database import session
 from .utils import upload_path
 
 
+@app.route("/api/songs", methods=["GET"])
+#@decorators.accept("application/json")
+def songs_get():
+    """ Get a list of songs """
+    # Get the querystring arguments
+    #name_like = request.args.get("name_like")
+
+    # Get and filter the songs from the database
+    songs = session.query(models.Song)
+    #if name_like:
+    #    songs = songs.filter(models.Song.name.contains(name_like))
+    songs = songs.order_by(models.Song.id)
+
+    # Convert the posts to JSON and return a response
+    data = json.dumps([song.as_dictionary() for song in songs])
+    return Response(data, 200, mimetype="application/json")
+
+
